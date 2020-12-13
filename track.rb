90a75907ae2dda60cad7bfbafc9c1c3553f04b6d
Rishabh <mailrishabh.ra@gmail.com>
2020-12-13 07:25:01 +0530

    staging commit

---
+++ track.rb
@@ -0,0 +1,10 @@
+class Track 
+    attr_reader :number, :sessions, :morning_session, :evening_session
+    
+    def initialize(number)
+        @number = number
+        @morning_session = Session.new(9,13)
+        @evening_session = Session.new(14,17)
+        @sessions = [@morning_session, @evening_session]
+    end
+end
